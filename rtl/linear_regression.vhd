59ed7002f7d0826a1a96ae4cdb6403865985fda5
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2018-06-05 13:23:53 -0300

    Feat: project.

---
+++ rtl/linear_regression.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity linear_regression is
+	port (
+		
+	)
+end linear_regression;
+
+architecture behavioral of linear_regression is
+begin
+	
+end behavioral;
---
+++ rtl/loss.vhd
@@ -0,0 +1,13 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity loss is
+	port (
+		
+	)
+end loss;
+
+architecture behavioral of loss is
+begin
+	
+end loss;
